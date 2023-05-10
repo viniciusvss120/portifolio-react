384286cbfa647030061530acebd7a1d437d353df
Vinicius Silva Souza <viniciusvss120@gmail.com>
2023-05-10 10:44:24 -0400

    Finalizando a seçao de projetos

--- src/habilidades/Habilidades.js
+++ src/habilidades/Habilidades.js
@@ -3,7 +3,39 @@ import style from './Habilidades.module.css'
 const Habilidades = () => {
   return (
     <div className={style.habilidades}>
-      <h1>habilidades</h1>
+      <h1>Habilidades</h1>
+      <ul className={style.nivel}>
+        <li>
+          <span>JavaScript: </span>
+          <div className={style.stack}><div style={{width: "90%"}}></div></div>
+          <span>90%</span>
+        </li>
+        <li>
+          <span>Node.js:</span>
+          <div className={style.stack}><div style={{width: "55%"}}></div></div>
+          <span>55%</span>
+        </li>
+        <li>
+          <span>Vue.js:</span>
+          <div className={style.stack}><div style={{width: "75%"}}></div></div>
+          <span>75%</span>
+        </li>
+        <li>
+          <span>React.js:</span>
+          <div className={style.stack}><div style={{width: "45%"}}></div></div>
+          <span>45%</span>
+        </li>
+        <li>
+          <span>Typescript:</span>
+          <div className={style.stack}><div style={{width: "40%"}}></div></div>
+          <span>40%</span>
+        </li>
+        <li>
+          <span>MySQL:</span>
+          <div className={style.stack}><div style={{width: "42%"}}></div></div>
+          <span>42%</span>
+        </li>  
+      </ul>
     </div>
   )
 }
--- src/projetos/Projetos.js
+++ src/projetos/Projetos.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import style from './Projetos.module.css'
+import alugel from '../img/alugel.png'
+import dogs from '../img/dogs.png'
+import flexbox from '../img/flexbox.png'
+import login from '../img/login.png'
+import mec from '../img/mec.png'
+import direita from '../img/direita.png'
+import esquerda from '../img/esquerda.png'
 
 const Projetos = () => {
   return (
-    <div>
+    <div className={style.projetos}>
       <h1>Projetos</h1>
+      <div className={style.container}>
+        <button className={style.left}>
+          <img src={esquerda} alt="esquerda" />
+        </button>
+        <button className={style.right}>
+          <img src={direita} alt="direita" />
+        </button>
+        <div className={style.gallery_wraper}>
+          <div className={style.gallery}>
+            <img className={`${style.item} ${style.currentItem}`} src={alugel} alt="alugel" />
+            <img className={`${style.item} ${style.currentItem}`} src={dogs} alt="dogs" />
+            <img className={`${style.item} ${style.currentItem}`} src={flexbox} alt="flexbox" />
+            <img className={`${style.item} ${style.currentItem}`} src={login} alt="login" />
+            <img className={`${style.item} ${style.currentItem}`} src={mec} alt="mec" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
