77c26a1c6e637b85a9d5bd0f2758eb42f52fed99
Vinicius Silva Souza <viniciusvss120@gmail.com>
2023-05-15 20:57:54 -0400

    Finalizando o efeito de scroll dos projetos

--- src/projetos/Projetos.js
+++ src/projetos/Projetos.js
@@ -5,60 +5,61 @@ import dogs from '../img/dogs.png'
 import flexbox from '../img/flexbox.png'
 import login from '../img/login.png'
 import mec from '../img/mec.png'
-import direita from '../img/direita.png'
-import esquerda from '../img/esquerda.png'
+// import direita from '../img/direita.png'
+// import esquerda from '../img/esquerda.png'
 
 const Projetos = () => {
-  
 
+  let currentItem = 0
   React.useEffect(() => {
     const btn = document.querySelectorAll(".btn")
-    let currentItem = 0
     const item = document.querySelectorAll(".img")
-    const maxItem = item.length
-
-    console.log(item)
-
-    btn.forEach(control => {
-      control.addEventListener("click", () => {
-        const isLeft = control.classList.contains("Projetos_left__b+r+q")
   
-        if(isLeft){
-          currentItem -= 1
-        }else{
-          currentItem += 1
-        }
-
-        if(currentItem >= maxItem){
-          currentItem = 0
-        }
-
-        if(currentItem < 0) {
-          currentItem = maxItem - 1
-        }
-        
-        item[currentItem].scrollIntoView({
-          inline: "center",
-          behavior: "smooth"
-        })
-        // item.forEach(index => {
+    if(btn.length > 0){
+      btn.forEach(control => {
+        control.addEventListener("click", () => {
           
-        // })
-        console.log(isLeft, currentItem)
-      })
-    })
-  }, [])
+          const maxItem = item.length
+          const isLeft = control.classList.contains("Projetos_left__b+r+q")
+    
+          if(isLeft){
+            currentItem -= 1
+          }else{
+            currentItem += 1
+          }
+    
+          if(currentItem >= maxItem){
+            currentItem = 0
+          }
+    
+          if(currentItem < 0) {
+            currentItem = maxItem - 1
+          }
+          
+          item[currentItem].scrollIntoView({       
+            block: "nearest",
+            inline: "center",
+            behavior: "smooth"
+          })
 
+          console.log(item[currentItem], currentItem)
+        })
+      })
+    }
+    
+  },[])
 
   return (
     <div className={style.projetos} id='projetos'>
       <h1>Projetos</h1>
       <div className={style.container}>
         <button className={`${style.left} btn`}>
-          <img src={esquerda} alt="esquerda" />
+          previw
+          {/* <img src={esquerda} alt="esquerda" /> */}
         </button>
         <button className={`${style.right} btn`}>
-          <img src={direita} alt="direita" />
+          next
+          {/* <img src={direita} alt="direita" /> */}
         </button>
         <div className={style.gallery_wraper}>
           <div className={style.gallery}>
@@ -66,6 +67,10 @@ const Projetos = () => {
             <img className={`${style.item} ${style.currentItem} img`} src={dogs} alt="dogs" />
             <img className={`${style.item} ${style.currentItem} img`} src={flexbox} alt="flexbox" />
             <img className={`${style.item} ${style.currentItem} img`} src={login} alt="login" />
+            <img className={`${style.item} ${style.currentItem} img`} src={mec} alt="mec" />
+            <img className={`${style.item} ${style.currentItem} img`} src={mec} alt="mec" />
+            <img className={`${style.item} ${style.currentItem} img`} src={mec} alt="mec" />
+            <img className={`${style.item} ${style.currentItem} img`} src={mec} alt="mec" />
             <img className={`${style.item} ${style.currentItem} img`} src={mec} alt="mec" />
           </div>
         </div>
